6b9e152898efea85d171c2610e09c88d16b834cc
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-25 23:34:16 +0530

    axios installed from npm for fetch datas

--- src/routes/movie-routes/allMovie.jsx
+++ src/routes/movie-routes/allMovie.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MovieCard from '../../components/movieCard'
 
+export async function loader() {
+    
+  }
 
 export default function AllMovies() {
   return (
     <>
-    <MovieCard/>
       
     </>
   )

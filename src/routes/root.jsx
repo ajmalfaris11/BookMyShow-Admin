ca45ee6bffd9be10fce668a6764353305cfda6a9
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-25 18:25:05 +0530

    root page basic view structure designed

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Navbar from '../components/navbar'
+import React from "react";
+import Navbar from "../components/navbar";
+import { Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
     <>
-     <Navbar />
+      <div className="px-2 py-3 h-lvh">
+        <Navbar />
+        <Outlet />
+      </div>
     </>
-  )
+  );
 }
